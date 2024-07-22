cc9eb7540269209005b1e5ab20e7d01579663f6f
aallen417 <austin.allen417@gmail.com>
2024-07-22 18:37:56 -0400

    adds eventlistener for clicking card elementsadds a handlecardclick function for selecting card and changing state to flip card

--- js/app.js
+++ js/app.js
@@ -1,37 +1,64 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const sortedDeck = ["hM", "hP", "hR", "hS", "hT", "hW", "hM", "hP", "hR", "hS", "hT", "hW"]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board
 let deck
+let card
+let turn
 let score
 let win
 
 
+let gameDeck = []
+let selectedCard1
+let selectedCard2
+
+
 /*------------------------ Cached Element References ------------------------*/
-const cardPlaceholderEl = document.querySelectorAll(".empty-card-placeholder")
+const cardEls = document.querySelectorAll(".card")
 const currentScoreEl = document.getElementById("current-score")
-
-
 
 /*-------------------------------- Functions --------------------------------*/
 easyDiffInit()
 
+
 function easyDiffInit() {
-    board = ["", "", "", "", "", "", "", "", "", "", "", "" ]
-    // deck = easyDeck
+    gameDeck = []
     currentScoreEl.textContent = 0 
     win = false
-    render()
+    turn = 1
+    cardEls.forEach((cardEl) =>  {
+    cardEl.className = "card large back-easy"
+    })
+    shuffleDeck()
+    render()   
+}
+
+
+
+function shuffleDeck() {
+    while (sortedDeck.length > 0) {
+        let randomIdx = Math.floor(Math.random() * sortedDeck.length)
+        let randomCard = sortedDeck.splice(randomIdx, 1)[0]
+        gameDeck.push(randomCard)
+        console.log(gameDeck)
+        }
+}
+
+function handleCardClick(cardEl, idx) {
+    console.log("clicked")
+    cardEl.className = `${"card large"} + ${gameDeck[cardEl.id]}`
 }
 
 function render() {
     
-}
+        }
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+cardEls.forEach((cardEl) => {
+    cardEl.addEventListener("click", event => {handleCardClick(cardEl)})
+})
 
 
