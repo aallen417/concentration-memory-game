b5c46de19c214252dc82979556c368a6580514f9
aallen417 <austin.allen417@gmail.com>
2024-07-24 14:18:56 -0400

    cleans up code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 const sortedDeck = ["hM", "hP", "hR", "hS", "hT", "hW", "hM", "hP", "hR", "hS", "hT", "hW"]
 
-
 /*---------------------------- Variables (state) ----------------------------*/
 let deck
 let card
@@ -10,12 +9,9 @@ let score
 let lives
 let win
 let defeat
-
-
 let gameDeck = []
 let selectedCard1
 let selectedCard2
-
 
 /*------------------------ Cached Element References ------------------------*/
 const cardEls = document.querySelectorAll(".card")
@@ -23,7 +19,6 @@ const currentScoreEl = document.getElementById("score")
 const currentLivesEl = document.getElementById("lives")
 /*-------------------------------- Functions --------------------------------*/
 easyDiffInit()
-
 
 function easyDiffInit() {
     gameDeck = []
@@ -39,17 +34,13 @@ function easyDiffInit() {
     render()   
 }
 
-
-
-
-
 function shuffleDeck() {
     while (sortedDeck.length > 0) {
         let randomIdx = Math.floor(Math.random() * sortedDeck.length)
         let randomCard = sortedDeck.splice(randomIdx, 1)[0]
         gameDeck.push(randomCard)
         console.log(gameDeck)
-        }
+    }
 }
 
 function handleCardClick(cardEl) {
@@ -70,29 +61,20 @@ function handleCardClick(cardEl) {
         turn = 1
     }
     checkForVictory()   
-    checkForDefeat()
-    console.log(selectedCard1)
-    console.log(selectedCard2)
-
-        
+    checkForDefeat()        
 }
 
 function compare(selectedCard1, selectedCard2) {
     if (gameDeck[selectedCard2.id] !== gameDeck[selectedCard1.id]) {
         lives -= 1
         setTimeout(() => selectedCard1.className = "card large back-easy", 2500)
-        setTimeout(() => selectedCard2.className = "card large back-easy", 2500)
-        console.log("not a match")
+        setTimeout(() => selectedCard2.className = "card large back-easy", 2500)        
         render()
     }   else {
             score += 5
-            console.log("Is a match")
             render()
-    }
-        console.log(selectedCard1)
-        console.log(selectedCard2)
+        }        
 }
-
 
 function checkForVictory() {
    if (score === 30) {
@@ -114,7 +96,6 @@ function render() {
     currentScoreEl.textContent = "Score: " + score
     currentLivesEl.textContent = "Lives: " + lives
     }
-
 
 /*----------------------------- Event Listeners -----------------------------*/
 cardEls.forEach((cardEl) => {
